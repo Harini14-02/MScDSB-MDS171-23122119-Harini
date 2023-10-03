3dfe697cf3439534131ca89e3562bebb7a16896e
Harini <hsruthi14@gmail.com>
2023-10-03 15:17:28 +0530

    update

--- Lab 07.py
+++ Lab 07.py
@@ -1,92 +1,52 @@
-
 class expenseTracker:
-    def __init__(self,file_name):
-        self.file_name = file_name
-
-    def write(self,msg):
-        with open('expense.txt','w') as myFile:
-            myFile.write(msg)
-log = expenseTracker('myNewFile.txt')
-log.write("this is a log file.")
-
-    
     def __init__(self):
-        self.expenseDict = {
-            "income": [],
-            "expense": [],
-        }
+        self.transactionDetails={"details":[]}
 
-    def store_transactions(self, type, amt, category, date, details):
-        trans = {
-            "Amount": amt,
-            "Category": category,
-            "Date": date,
-            "Details": details,
-        }
-        if type == "income":
-            self.expenseDict['income'].append(trans)
-        else:
-            self.expenseDict['expense'].append(trans)
+    def retrieveTransactions(self):
+        for i in open("Expense_Income_Tracker.csv","r+").readlines():
+            line=i.split(",")
+            if line[1]!="Expense Category":
+                transaction={"type":line[0],"category":line[1],"amount":line[2],"description":line[3],"date":line[4]}
+                self.transactionDetails["details"].append(transaction)
 
-    def view_transactions(self):
-        print("Your Income:")
-        for item in self.expenseDict['income']:
-            print(item)
-        print("Your Expenses:")
-        for item in self.expenseDict['expense']:
-            print(item)
-
-    def calculate_transactions(self):
-        total_income = 0
-        for item in self.expenseDict['income']:
-            total_income += item["Amount"]
-        print("Total Income\t:\t", total_income)
-
-        total_expense = 0
-        for item in self.expenseDict['expense']:
-            total_expense += item["Amount"]
-        print("Total Expenses\t:\t", total_expense)
-
-# define a menu that will let users to enter expense, view expenses
-# or income, get totals in each and exit from the program
-
-
-def collectInput():
-    amt = int(input("Enter the amout: "))
-    category = input("Enter Category: ")
-    date = input("Enter Date (DD/MM/YYYY): ")
-    details = input("Enter description: ")
-    return amt, category, date, details
-
-
-myexpense = expenseTracker()
-
-while True:
-    print("...MY EXPENSE TRACKER...")
-    print("1. Record Income")
-    print("2. Record Expense")
-    print("3. View Records")
-    print("4. View My Spendings")
-    print("5. Exit")
-
-    choice = int(input("Enter your choice: ").strip())
-
-    if choice == 1:
-        print("Enter the details of income")
-        amt, category, date, details = collectInput()
-        myexpense.store_transactions("income", amt, category, date, details)
-    elif choice == 2:
-        print("Enter the details of expense")
-        amt, category, date, details = collectInput()
-        myexpense.store_transactions("expense", amt, category, date, details)
-    elif choice == 3:
-        myexpense.view_transactions()
-    elif choice == 4:
-        myexpense.calculate_transactions()
-    elif choice == 5:
-        exit()
-    else:
-        print("In valid choice")
-
+    def calculateTotal(self):
+        totalIncome=0
+        totalExpense=0
+        for i in self.transactionDetails["details"]:
+            if i["type"]=="Income":
+                totalIncome+=int(i["amount"])
+            else:
+                totalExpense+=int(i["amount"])
+        return totalIncome,totalExpense
     
+    def addTransaction(self,type,category,amount,description,date):
+        transaction={"type":type,"category":category,"amount":amount,"description":description,"date":date}
+        self.transactionDetails["details"].append(transaction)
+    
+    def writeTransactions(self):
+        file=open("Expense_Income_Tracker.csv","w+")
+        file.write("Type,Expense Category,Amount,Description,Date\n")
+        for i in self.transactionDetails["details"]:
+            date=str(i["date"]).strip()
+            file.write(i["type"]+","+i["category"]+","+i["amount"]+","+i["description"]+","+date+"\n")
+        file.close()
 
+
+order=expenseTracker()
+order.retrieveTransactions()
+while True:
+    print("1.Add New Transaction\n2.Calculate Total Income and Expense\n3.Exit")
+    choice=int(input("Select your action:"))
+    if choice==1:
+        type=input("Enter the type of transaction (Income/Expense):")
+        category=input("Enter the category:")
+        amount=input("Enter the amount:")
+        description=input("Enter the description of transaction:")
+        date=input("Enter the date mm/dd/yyyy:")
+        order.addTransaction(type,category,amount,description,date)
+    elif choice==2:
+        totalIncome,totalExpense=order.calculateTotal()
+        print("Total Income=",totalIncome,"\nTotal Expense=",totalExpense)
+        order.writeTransactions()
+    elif choice==3:
+        break
